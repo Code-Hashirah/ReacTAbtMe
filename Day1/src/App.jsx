70d9c353b65b8288cdaf55c9671497793c2a35b3
Kabir <kabirajibade@yahoo.com>
2023-05-09 14:02:07 +0100

    first react commit

---
+++ Day1/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+   
+      <div style={{
+        backgroundColor:'black',
+        color:'lightblue',
+        width:'100%'
+      }}>
+        <section>
+          <h1>Kabir</h1>
+          <img src="http://images6.fanpop.com/image/photos/36500000/Kakashi-image-kakashi-36592939-1892-2621.jpg" alt="kabir" style={{
+            width:'150px',
+            height:'150px',
+          }}/>
+          <p style={{width:'100%'}}>
+                  I am Abba, a Nigerian male,
+              My skills in web development, I love to unveil,
+              Fullstack is my expertise, a talent I embrace,
+              In code, I find joy, in solving problems, my space.
+          </p>
+          <p>
+            
+          My faith, my Islam, a guide for me,
+              A moral compass, my values to see,
+              In prayer, I find peace, in service, I find grace,
+              My identity, a part of me, I happily embrace.
+          </p>
+
+          <p>
+          As a Nigerian, my culture, my roots,
+              A heritage, so rich, a story that shoots,
+              I carry it with pride, in all that I do,
+              My diversity, a strength, a perspective so true.
+          </p>
+
+          <p>
+          Abba, my name, a rhythm so fine,
+              A unique identity, that is truly mine,
+              As a web developer, as a Muslim Nigerian male,
+              My journey, my story, I am proud to unveil.
+          </p>
+        </section>
+      </div>
+   
+  )
+}
+
+export default App
